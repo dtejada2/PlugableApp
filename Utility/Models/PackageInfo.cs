d62df1a3611a570360f42ff2c342b0714d87f331
Daniel Tejada <DanielTejada65@hotmail.com>
2018-04-19 22:09:34 -0400

    - Adding installer  and uninstaller package - Adding package info model

---
+++ Utility/Models/PackageInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Models
+{
+    public class PackageInfo
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string FullName { get; set; }
+        public Version CurrentVersion { get; set; }
+        public Version ServerVersion { get; set; }
+
+
+        public string CurrentVersionString { get { return CurrentVersion != null ? CurrentVersion.ToString() : ""; } }
+        public string ServerVersionString { get { return ServerVersion != null ? ServerVersion.ToString() : ""; } }
+        public string VersionAction
+        {
+            get
+            {
+                if (CurrentVersion == null || ServerVersion == null)
+                    return "";
+
+                var result = CurrentVersion.CompareTo(ServerVersion);
+
+                if (result < 0)
+                {
+                    return "Update";
+                }
+                else if (result == 0)
+                {
+                    return "No action";
+                }
+                else
+                {
+                    return "Downgrade";
+                }
+            }
+        }
+    }
+}
--- Utility/NugetPackageManager.cs
+++ Utility/NugetPackageManager.cs
@@ -8,12 +8,17 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.Versioning;
 using System.Windows.Forms;
+using System.ComponentModel;
+using System.Text;
+using System.Diagnostics;
 
 namespace Utility
 {
     public class NugetPackageManager : INugetPackageManager
     {
         IPackageRepository _packageRepository;
+        string rootAppDirectory;
+
         public NugetPackageManager()
         {
             string _nugetServer = Common.Config.nugetServer;
@@ -37,6 +42,71 @@ namespace Utility
             }
             //Iterate through the list and print the full name of the pre-release packages to console
             return packages.ToList();
+        }
+
+        public void InstallPackage(string packageId, string version)
+        {
+            DownLoadPackage(packageId, version);
+
+            AddPackageConfig(packageId, SemanticVersion.Parse(version));
+        }
+
+        public void UninstallPackage(string packageId, string version)
+        {
+            KillAllProcessesBelongingToPackage();
+
+            //TODO check if the dll or exe is in use
+            if (true)
+                RemovePackageConfig(packageId, SemanticVersion.Parse(version));
+        }
+
+        public void KillAllProcessesBelongingToPackage()
+        {
+            var ourExe = Assembly.GetEntryAssembly();
+            var ourExePath = ourExe != null ? ourExe.Location : null;
+
+            UnsafeUtility.EnumerateProcesses()
+                .Where(x =>
+                {
+                    // Processes we can't query will have an empty process name, we can't kill them
+                    // anyways
+                    if (String.IsNullOrWhiteSpace(x.Item1)) return false;
+
+                    // Files that aren't in our root app directory are untouched
+                    if (!x.Item1.StartsWith(rootAppDirectory, StringComparison.OrdinalIgnoreCase)) return false;
+
+                    // Never kill our own EXE
+                    if (ourExePath != null && x.Item1.Equals(ourExePath, StringComparison.OrdinalIgnoreCase)) return false;
+
+                    var name = Path.GetFileName(x.Item1).ToLowerInvariant();
+                    if (name == "Update.exe") return false;
+
+                    return true;
+                })
+                .ForEach(x =>
+                {
+                    try
+                    {
+                        Process.GetProcessById(x.Item2).Kill();
+                        //this.WarnIfThrows(() => Process.GetProcessById(x.Item2).Kill());
+                    }
+                    catch { }
+                });
+        }
+
+        private void RemoveFile(string packageId)
+        {
+            var file = LocatePackage(packageId);
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+        }
+
+        //TODO
+        private string LocatePackage(string packageId)
+        {
+            return "";
         }
 
         public void DownLoadPackage(string packageId, string version)
@@ -95,6 +165,50 @@ namespace Utility
         {
             var file = GetPackageReferenceFile();
             return file.GetPackageReferences();
+        }
+    }
+
+    static unsafe class UnsafeUtility
+    {
+        public static List<Tuple<string, int>> EnumerateProcesses()
+        {
+            int bytesReturned = 0;
+            var pids = new int[2048];
+
+            fixed (int* p = pids)
+            {
+                if (!NativeMethods.EnumProcesses((IntPtr)p, sizeof(int) * pids.Length, out bytesReturned))
+                {
+                    throw new Win32Exception("Failed to enumerate processes");
+                }
+
+                if (bytesReturned < 1) throw new Exception("Failed to enumerate processes");
+            }
+
+            return Enumerable.Range(0, bytesReturned / sizeof(int))
+                .Where(i => pids[i] > 0)
+                .Select(i => {
+                    try
+                    {
+                        var hProcess = NativeMethods.OpenProcess(ProcessAccess.QueryLimitedInformation, false, pids[i]);
+                        if (hProcess == IntPtr.Zero) throw new Win32Exception();
+
+                        var sb = new StringBuilder(256);
+                        var capacity = sb.Capacity;
+                        if (!NativeMethods.QueryFullProcessImageName(hProcess, 0, sb, ref capacity))
+                        {
+                            throw new Win32Exception();
+                        }
+
+                        NativeMethods.CloseHandle(hProcess);
+                        return Tuple.Create(sb.ToString(), pids[i]);
+                    }
+                    catch (Exception)
+                    {
+                        return Tuple.Create(default(string), pids[i]);
+                    }
+                })
+                .ToList();
         }
     }
 }
