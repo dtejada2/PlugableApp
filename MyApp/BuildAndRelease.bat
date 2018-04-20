cad8717207737dea9c9e130e208dceab20a276d1
Daniel Tejada <DanielTejada65@hotmail.com>
2018-04-20 21:33:47 +0000

    Deleted MyApp

--- MyApp/BuildAndRelease.bat
+++
@@ -1,8 +0,0 @@
-Set-Variable -Name "version" -Value "1.2.2"
-
-nuget pack -Version $version
-
-squirrel.exe --releasify MyApp.$version.nupkg
-
-nuget.exe push MyApp.$version.nupkg dsfdsfsdfsdfsdfsdfsd -Source http://localhost:9000/nuget
- 
