1faf8bbd756f28d0af97e7f64b96e9ab20fec00b
Daniel Tejada <DanielTejada65@hotmail.com>
2018-04-20 21:33:32 +0000

    Deleted CustomerPlug

--- CustomerPlug/Build.ps1
+++
@@ -1,8 +0,0 @@
-﻿Set-Variable -Name "version" -Value "1.0.0"
-
-nuget pack -Version $version
-
-squirrel.exe --releasify MyApp.$version.nupkg
-
-nuget.exe push MyApp.$version.nupkg dsfdsfsdfsdfsdfsdfsd -Source http://localhost:9000/nuget
- 
